2fca826f5a80c9b19fc593e48e9f020d88ffa25e
Carlos Gonzales <gonzalescarlos638@gmail.com>
2019-03-09 01:03:37 -0500

    adding more javasript

--- main.js
+++ main.js
@@ -3,4 +3,25 @@ const TypeWriter = function(txtElement, words, wait = 3000) {
     this.words = words;
     this.txt = '';
     this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+// Type Method 
+TypeWriter.prototype.type = function() {
+    console.log('Hello');
+
+    setTimeout((() => this.type(), 500)
+}
+// Initialize when DOM loads 
+document.addEventListener('DOMContentLoaded', init);
+
+// Initialize App
+function init() {
+   const txtElement = document.querySelector('.txt-type');
+   const words = JSON.parse(txtElement.getAttribute('data-words'));
+   const wait = txtElement.getAttribute('data-wait');
+// Initialize Typewriter
+new TypeWriter(txtElement, words, wait);
 }
